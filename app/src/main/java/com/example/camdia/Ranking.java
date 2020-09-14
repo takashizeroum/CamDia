c2741488f61a80bf7e219cdd6ca7c796e9e39c1a
takashinagata.thiago@gmail.com <Takashi71@>
2020-09-14 18:34:43 -0300

    Versão Nova de Layouts

---
+++ app/src/main/java/com/example/camdia/Ranking.java
@@ -0,0 +1,14 @@
+package com.example.camdia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ranking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+    }
+}
---
+++ app/src/main/java/com/example/camdia/TDL.java
@@ -0,0 +1,14 @@
+package com.example.camdia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TDL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t_d_l);
+    }
+}
