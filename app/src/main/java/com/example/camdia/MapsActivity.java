ad70acc128c91bedf9772c3bfef68e9335909bd1
takashinagata.thiago@gmail.com <Takashi71@>
2020-09-29 01:56:48 -0300

    avanços

--- app/src/main/java/com/example/camdia/MapsActivity.java
+++ app/src/main/java/com/example/camdia/MapsActivity.java
@@ -1,13 +1,27 @@
 package com.example.camdia;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -15,14 +29,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    //Permissão
+
+    private String[] permissoes = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Permissoes.validarPermissoes(permissoes,this,1);
+
+        locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.d("Localização", "onLocationChanged: "+location.toString());
+            }
+        };
+        
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int permissaoResultado : grantResults){
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED){
+                //mostrar alerta de falha na autorização de local
+                alertDesacordoGps();
+            }
+            else if (permissaoResultado == PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                }
+
+            }
+        }
+
     }
 
     /**
@@ -37,10 +89,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        // Add a marker in Sydney and move the camera
-        LatLng etec = new LatLng(-23.702723, -46.6914657);
-        mMap.addMarker(new MarkerOptions().position(etec).title("Etec irmã agostina"));
+
+        //Teste marcador Marcador Inicial
+
+        LatLng etec = new LatLng(-23.702723, -46.6898242);
+        mMap.addMarker(new MarkerOptions().position(etec).title("Etec irmã agostina").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(etec,15));
+
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                Double lat, lon;
+                lat = latLng.latitude;
+                lon = latLng.longitude;
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("Latitude:"+lat+" Longitude:"+lon)
+                        .icon(BitmapDescriptorFactory
+                                .fromResource(R.drawable.markflag)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+
+            }});
+       }
+
+    private void alertDesacordoGps (){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissão Negada");
+        builder.setMessage("Para utilizar o aplicativo de Caminhadas é necessário aceitar a permissão de Localização.");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Prosseguir", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
+
+
 }
+
